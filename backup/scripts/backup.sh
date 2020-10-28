b04ffce4448b1d3fcd6e10ad29fbab3026a9432f
k_appadurai <k.appadurai@spglobal.com>
2020-10-28 17:27:50 -0400

    added scripts folder

---
+++ backup/scripts/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /data/backup
+
+filename="asset_backup_$(date +%Y%m%dT%H%M).tar.gz"
+echo filename: $filename
+echo BACKUP_S3_BUCKET: $BACKUP_S3_BUCKET
+
+tar -cvzf $filename asset
+
+aws s3 cp $filename s3://$BACKUP_S3_BUCKET
