1c466556814dd0c8ee636b401f1b5ee300bc5f94
k_appadurai <k.appadurai@spglobal.com>
2020-10-29 21:34:09 -0400

    fixed folder name

--- backup/scripts/backup.sh
+++ backup/scripts/backup.sh
@@ -6,6 +6,6 @@ filename="asset_backup_$(date +%Y%m%dT%H%M).tar.gz"
 echo filename: $filename
 echo BACKUP_S3_BUCKET: $BACKUP_S3_BUCKET
 
-tar -cvzf $filename /data/scheduler
+tar -cvzf $filename -C /data scheduler
 
 aws s3 cp $filename s3://$BACKUP_S3_BUCKET
