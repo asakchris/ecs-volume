6e609923726b744d9e4af274e610d1fbdf27c92f
k_appadurai <k.appadurai@spglobal.com>
2020-10-29 22:05:34 -0400

    removed zip

--- backup/scripts/backup.sh
+++ backup/scripts/backup.sh
@@ -2,10 +2,10 @@
 
 cd /data/backup
 
-filename="asset_backup_$(date +%Y%m%dT%H%M).tar.gz"
+filename="asset_backup_$(date +%Y%m%dT%H%M).tar"
 echo filename: $filename
 echo BACKUP_S3_BUCKET: $BACKUP_S3_BUCKET
 
-tar -cvzf $filename -C /data scheduler
+tar -cvf $filename -C /data scheduler
 
 aws s3 cp $filename s3://$BACKUP_S3_BUCKET
