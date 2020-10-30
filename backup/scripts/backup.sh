7cb9f6c77bbc2c5f3a044dfaea3817be9636870c
k_appadurai <k.appadurai@spglobal.com>
2020-10-30 09:27:49 -0400

    delete backup file after pushing to S3

--- backup/scripts/backup.sh
+++ backup/scripts/backup.sh
@@ -6,6 +6,11 @@ filename="asset_backup_$(date +%Y%m%dT%H%M).tar"
 echo filename: $filename
 echo BACKUP_S3_BUCKET: $BACKUP_S3_BUCKET
 
+# take backup
 tar -cvf $filename -C /data scheduler
 
+# push backup into S3
 aws s3 cp $filename s3://$BACKUP_S3_BUCKET
+
+# delete backup
+rm $filename
